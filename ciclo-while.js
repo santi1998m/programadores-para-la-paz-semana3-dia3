d493e4733e25a9df792b67688ac6bed5c1258a25
santi1998m <162331952+santi1998m@users.noreply.github.com>
2026-04-30 21:04:46 -0500

    Modify while loop to count up to 5
    Updated the while loop to iterate 5 times and changed the variable from 'intento' to 'numero'.

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,13 +1,6 @@
-let intento = 1
-const maximoIntentos = 3
+let numero = 1;
 
-console.log("Práctica inicial con while")
-console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
-
-while (intento <= maximoIntentos) {
-  console.log("Intento de revisión número:", intento)
-  intento = intento + 1
+while (numero <= 5) {
+  console.log("Revisión número " + numero);
+  numero++;
 }
-
-console.log("Proceso de revisión finalizado.")
-// cambio
